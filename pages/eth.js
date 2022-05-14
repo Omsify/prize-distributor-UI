b0b52ab5985111667373d5770846313da7056cf3
OmiAkk <badzan00@mail.ru>
2022-05-14 10:25:47 +0500

    Added network change

--- pages/eth.js
+++ pages/eth.js
@@ -116,6 +116,21 @@ const PrizeDistribution = () => {
                 const accounts = await web3.eth.getAccounts()
                 setAddress(accounts[0])
 
+                /* Change network to Rinkeby */
+                await ethereum.request({
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: web3.utils.toHex('4') }],
+                })
+                    .then(() => console.log('network has been set'))
+                    .catch((e) => {
+                        if (e.code === 4902) {
+                            console.log('network is not available, add it')
+                        } else {
+                            console.log('could not set network')
+                        }
+                    })
+
+
                 /* Create local contract copy */
                 const pdContract = prizeDistributorContract(web3)
                 setPdContract(pdContract)
